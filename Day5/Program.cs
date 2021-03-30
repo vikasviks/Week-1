cf3b09ef5088dd402d709cb8fedc84bab5e8cbd9
vikasviks <67867372+vikasviks@users.noreply.github.com>
2021-03-31 00:22:48 +0530

    Day 5

---
+++ Day5/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter your name");
+            var name=Console.ReadLine();
+            var date=DateTime.Now;
+            Console.WriteLine($"hello{name} on {date}");
+            Console.ReadKey(true);
+        }
+    }
+}
